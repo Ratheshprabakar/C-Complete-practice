d04c809b9d64f330a609353743495a36af54d04c
Ratheshprabakar <ratheshprabakar@gmail.com>
2020-07-11 16:16:41 +0530

    Added 10.c

---
+++ 10.c
@@ -0,0 +1,14 @@
+//Program to check whether the number is even or add
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("Enter the number\t");
+	scanf("%d",&n);
+	if(n%2==0)
+	{
+		printf("The Number is Even\t");	
+	}
+	else
+		printf("The Number is Odd\t");
+}
