01e8c9795323bf533a5726c95ff9a9d79e2ac513
Ratheshprabakar <ratheshprabakar@gmail.com>
2020-07-11 12:54:50 +0530

    Added 5.c

---
+++ 5.c
@@ -0,0 +1,9 @@
+//Program to accept value of radius and print area of a circle
+#include<stdio.h>
+int main()
+{
+	int r;
+	printf("Enter the radius of the circle\t");
+	scanf("%d",&r);
+	printf("The Area of a circle\t%f",(3.14*r*r));
+}
