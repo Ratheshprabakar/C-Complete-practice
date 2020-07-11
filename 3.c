a0bc50de900572ed6cf86f5c355de98228d5948c
Ratheshprabakar <ratheshprabakar@gmail.com>
2020-07-11 12:31:13 +0530

    Added 3.c

---
+++ 3.c
@@ -0,0 +1,9 @@
+//Program to accept values of two numbers and print their addition
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("Enter Two values\n");
+	scanf("%d\n%d",&a,&b);
+	printf("The sum is \t%d",a+b);
+}
