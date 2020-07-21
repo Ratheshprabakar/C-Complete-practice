e52beb52cc68571dff9aadaf86583576a29eb2bf
Ratheshprabakar <ratheshprabakar@gmail.com>
2020-07-21 23:27:20 +0530

    Added 43.c

---
+++ 43.c
@@ -0,0 +1,9 @@
+// Program to find the square root of a number
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("Enter the Number\t");
+	scanf("%d",&n);
+	printf("The Squar root of %d is %.2f",n,sqrt(n));
+}
