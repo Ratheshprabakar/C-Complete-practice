8015c7e9292076f0913098a35dc05ccd09424586
Ratheshprabakar <ratheshprabakar@gmail.com>
2020-07-31 20:15:05 +0530

    Added 76.c

--- Hard/76.c
+++ Hard/76.c
@@ -1,1 +1,18 @@
-//
+//Program to reverse the string using pointer
+#include<stdio.h>
+int main()
+{
+	char string[100],*p;
+	int i;
+	printf("Enter the string\t");
+	gets(string);
+	p=string;
+	for(i=0;i<strlen(string);i++)p++;
+	printf("The Reversed string is \t");
+	for(i=strlen(string);i>=0;i--)
+		printf("%c",*p--);
+
+	
+		
+	
+}
